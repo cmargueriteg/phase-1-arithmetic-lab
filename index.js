9f3302924c42ca4603e14520a5166cbdc9399102
Cindy Germain <cindymarguerite@gmail.com>
2022-02-24 19:45:27 -0700

    commit tree 7b166107dc20d64b10028a3772747f16d11fb939

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1=31;
+const num2=2;
+let multiply=num1 * num2;
+multiply;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+const num3=12;
+const num4=8;
+
+let mod= (num3 % num4);
+mod;  
+
+let max= Math.max(1,2,7,20,4);
+max; 
